db0b5356abbe818e242e0c66948b4915dd65391e
JP Linardon <jplinardon@yahoo.com>
2020-12-14 22:21:09 -0500

    Initial commit of node app

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchElement.value
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+        
+    })
+})
+})
